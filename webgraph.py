445198fc8436c81ab81b1161461a27264a4c50c4
Stefan Fernandez <stefan.fernandez@gmail.com>
2014-11-01 17:34:14 -0500

    added hashing

--- webgraph.py
+++ webgraph.py
@@ -3,17 +3,20 @@ import time
 import glob
 import re
 import networkx as nx
+import hashlib
+
 
 def natural_key(string_):
     """See http://www.codinghorror.com/blog/archives/001018.html"""
     return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
 
 start_time = time.clock()
 
 files = glob.glob("datasets/enron/*.txt")
 sorted_files = sorted(files, key=natural_key)
 
-#Create directed graph in networkx
+# Create directed graph in networkx
 arrayOfGraphs = []
 arrayOfPR = []
 for file in sorted_files:
@@ -32,33 +35,26 @@ for file in sorted_files:
         G.add_edge(nodeA, nodeB)
 
     pr = nx.pagerank(G)
+    #Iterate through edges to add to pagerank list
+    for a, b in G.edges_iter():
+        print str(a) + ":" + str(b) + "  " + str(len(G.edges(a)))
+        pr[str(a) + ":" + str(b)] = len(G.edges(a)) * pr[a]  #THIS NEEDS TO BE IMPROVED!!!
+        myhash = hashlib.md5(str(a) + str(b)).hexdigest()
+        hashbinary = bin(int(myhash, 16))[2:]
+        print hashbinary
+
+    binary_list = []
+    for d in hashbinary:
+        if d == 1:
+            binary_list.append(pr[])
+        else:
+            binary_list.append()
+
     arrayOfGraphs.append(G)
     arrayOfPR.append(pr)
 
-#Define data structures
-Array_Of_Adj_Lists = []
-
-for file in files:
-    f = open(file, "r")
-    dataGraph = f.readlines()
-    adjacency_List = []
-
-    for line in dataGraph:
-        nodeA, nodeB = line.split(" ")
-        nodeA.rstrip('\r\n')
-        nodeB.rstrip('\r\n')
-        nodeB = nodeB[:-1]
-        nodeA = int(nodeA)
-        nodeB = int(nodeB)
-
-        adjacency_List.append([nodeA, nodeB])
-
-    Array_Of_Adj_Lists.append(adjacency_List)
-
-
-print Array_Of_Adj_Lists
-
 #Calculate Quality Scores
+print arrayOfPR
 
 
 
@@ -73,4 +69,4 @@ for x in output.itervalues():
     f.write("\n")
 
 print "Complete"
-print("--- %s seconds ---" %( time.clock() - start_time))
+print("--- %s seconds ---" % ( time.clock() - start_time))
