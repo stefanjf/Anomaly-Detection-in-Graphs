b2902740d7d2e257f8b7d3145f5929fbf5e51214
Stefan Fernandez <stefan.fernandez@gmail.com>
2014-11-01 18:50:18 -0500

    calculates fingerprint

--- webgraph.py
+++ webgraph.py
@@ -13,18 +13,18 @@ def natural_key(string_):
 
 start_time = time.clock()
 
-files = glob.glob("datasets/enron/1*.txt")
+files = ["datasets/enron/0_enron_by_day.txt","datasets/enron/1_enron_by_day.txt"]#glob.glob("datasets/enron/1*.txt")
 sorted_files = sorted(files, key=natural_key)
 
 # Create directed graph in networkx
-arrayOfGraphs = []
-arrayOfPR = []
+pageRankNodesEdges = []
 
 for file in sorted_files:
     f = open(file, "r")
     dataGraph = f.readlines()
     G = nx.DiGraph()
 
+    #Add nodes to Graph object
     for line in dataGraph:
         nodeA, nodeB = line.split(" ")
         nodeA.rstrip('\r\n')
@@ -32,32 +32,44 @@ for file in sorted_files:
         nodeB = nodeB[:-1]
         nodeA = int(nodeA)
         nodeB = int(nodeB)
+        G.add_edge(str(nodeA), str(nodeB))
 
-        G.add_edge(nodeA, nodeB)
-
+    #Add edges to pagerank object
     pr = nx.pagerank(G)
-    #Iterate through edges to add to pagerank list
+    # Iterate through edges to add to pagerank list
     for a, b in G.edges_iter():
         print str(a) + ":" + str(b) + "  " + str(len(G.edges(a)))
         pr[str(a) + ":" + str(b)] = len(G.edges(a)) * pr[a]  #THIS NEEDS TO BE IMPROVED!!!
 
-    arrayOfGraphs.append(G)
-    arrayOfPR.append(pr)
+    pageRankNodesEdges.append(pr)
 
-for key, value in pr.iteritems():
-    myhash = hashlib.md5(str(key) + str(value)).hexdigest()
-    hashbinary = bin(int(myhash, 16))[2:]
-    print hashbinary
+    for key, value in pr.iteritems():
+        myhash = hashlib.md5(str(key) + str(value)).hexdigest()
+        hashbinary = bin(int(myhash, 16))[2:]
+        pr[hashbinary] = pr[key]
+        del pr[key]
 
-#Calculate w list
-binary_list = []
-for d in hashbinary:
-    if d == 1:
-        binary_list.append()
+
+# Calculate w list
+FINAL_list = []
+for pr_dict in pageRankNodesEdges:
+    for key, val in pr_dict.iteritems():
+        binary_list = []
+        for d in key:
+            if int(d) == 1:
+                binary_list.append(val)
+            else:
+                binary_list.append(-val)
+        FINAL_list.append(binary_list)
+
+file_fingerprint = [sum(x) for x in zip(*FINAL_list)]
+for index, item in enumerate(file_fingerprint):
+    if item > 0:
+        file_fingerprint[index] = 1
     else:
-        binary_list.append()
+        file_fingerprint[index] = 0
 
-
+print file_fingerprint
 
 
 
